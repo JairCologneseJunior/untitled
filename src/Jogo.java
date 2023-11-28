2747118221c04613263693eeaeeece0f57cb0786
JAIR COLOGNESE JUNIOR <jair.colognese@univille.br>
2023-11-28 20:24:56 -0300

    Initial commit

---
+++ src/Jogo.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Jogo {
+    List<String> Palavras;
+    List<String>Letras;
+    List<String>listasResposta;
+    public static void letraSorteada(){};
+    public static void validacaoPalavra(){};
+    public static void acumularPalavra(){};
+
+}
---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+        System.out.printf("Olá, bem-vindo ao jogo\nAdivinhe o máximo de palavras que começem com a letra\nVocê tem\ntempo\nDigite 1 para começar\nDigite 2 para sair");
+        numero = scanner.nextInt();
+        switch (numero) {
+            case 1:
+                System.out.printf("adad");
+                break;
+            case 2:
+                System.out.printf("adasda");
+                break;
+        }
+
+    scanner.close();
+    }
+}
